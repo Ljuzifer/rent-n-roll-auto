0693328423bc88dbc988fbe12a13ffad330c817d
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-12-10 20:36:07 +0200

    fix n/f

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 
 export const FooterBlock = styled.footer`
+    display: flex;
     background-color: #333;
     color: #fff;
     text-align: center;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,14 +5,11 @@ export default function NotFound() {
         <SectionContainer>
             <div
                 style={{
-                    display: "flex",
-                    flexDirection: "column",
                     textAlign: "center",
                     marginLeft: "auto",
                     marginRight: "auto",
-                    height: "100%",
+                    height: "100vh",
                     paddingTop: 200,
-                    paddingBottom: "auto",
                 }}>
                 <h1 style={{ fontSize: 120 }}>404</h1>
                 <b style={{ fontSize: 28 }}>Sorry, we couldn`t find that page...</b>
