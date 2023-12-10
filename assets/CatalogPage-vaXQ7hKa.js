b9f76c9f85bf453c77a78263f5295d524cb7fd63
Ljuzifer <Ljuzifer@users.noreply.github.com>
2023-12-10 18:16:44 +0000

    deploy: d3c3ba0737b6f734ebf4b9de3c95b7d2ab584702

---
+++ assets/CatalogPage-vaXQ7hKa.js
@@ -0,0 +1,22 @@
+import{s as d,j as t,r as l,S as c,u as x,a as i,b as h,c as f,d as m,f as g,e as j,M as C,L as b}from"./index-_0fY2oK9.js";import{c as y,C as L,a as F,F as S}from"./filterHelper-nlsp9W_a.js";const T=d.button`
+    display: block;
+    color: rgba(52, 112, 255, 1);
+
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.5;
+    text-decoration-line: underline;
+    border: none;
+    background: transparent;
+    margin: 0 auto;
+    transition: color 0.3s;
+
+    margin-top: 100px;
+
+    &:hover,
+    &:active,
+    &:focus {
+        color: rgba(11, 68, 205, 1);
+    }
+`;function p({loadMore:s}){return t.jsx(T,{style:{display:"block",marginLeft:"auto",marginRight:"auto",marginBottom:"150px"},type:"button",onClick:s,children:"Load more"})}p.propTypes;const k=12;function u({catalog:s}){const[n,a]=l.useState(1),e=n*k,o=()=>a(r=>r+1);return t.jsxs(t.Fragment,{children:[t.jsxs(c,{children:[t.jsx("ul",{style:{display:"flex",flexWrap:"wrap",columnGap:"29px",rowGap:"50px"},children:y(s,e).map(r=>t.jsx("li",{children:t.jsx(L,{car:r})},r.id))}),(s==null?void 0:s.length)>e&&t.jsx(p,{loadMore:o})]}),s.length===0&&t.jsx(c,{children:t.jsx("div",{children:t.jsx("b",{children:"Cars Not Found 😊 Try another options."})})})]})}u.propTypes;function M(){const s=x(),n=i(h),a=i(f),e=i(m),o=F(n,a);return l.useEffect(()=>{s(g())},[s]),l.useEffect(()=>{s(j(null))},[s]),t.jsxs(C,{children:[e?t.jsx(b,{type:"Gear",color:"blue",size:"220px"}):t.jsx(S,{cars:o}),n.length!==0&&!e&&t.jsx(u,{catalog:o})]})}export{M as default};
---
+++ assets/HomePage-0MB216C0.js
@@ -0,0 +1,67 @@
+import{s as t,j as e,S as n,M as a}from"./index-_0fY2oK9.js";const i=t.div`
+    font-weight: 500;
+    font-size: 55px;
+    letter-spacing: -13px;
+    width: 100%;
+    height: 666px;
+    background-color: grey;
+    background-image: linear-gradient(to right, rgba(46, 47, 66, 0.4), rgba(46, 47, 66, 0.4)),
+        url(${r=>r.font});
+    background-size: cover;
+    background-position: center;
+    border-radius: 22px;
+    color: white;
+    text-shadow: 8px 8px 18px rgba(0, 0, 0, 0.8);
+    text-align: center;
+    padding: 100px 20px;
+    margin-bottom: 20px;
+
+    span {
+        font-weight: 800;
+        font-size: 88px;
+        letter-spacing: -4px;
+        color: rgba(11, 68, 205, 1);
+        text-shadow: 8px 8px 18px rgba(0, 0, 0, 1);
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 44px;
+        letter-spacing: 2px;
+        text-shadow: 8px 8px 18px rgba(255, 255, 255, 0.8);
+        color: black;
+    }
+`,o=t.div`
+    width: 100%;
+    border-radius: 22px;
+    margin: 20px auto;
+    padding: 20px;
+    color: white;
+    text-align: center;
+    background-color: rgba(52, 112, 255, 1);
+    background-image: linear-gradient(to right, rgba(46, 47, 66, 0.4), rgba(46, 47, 66, 0.4)),
+        url(${r=>r.img});
+    box-shadow: 0 0 13px rgba(0, 0, 0, 0.8);
+
+    p {
+        line-height: 2.2;
+    }
+
+    h3 {
+        height: 100px;
+        color: rgba(0, 0, 0, 0.9);
+        font-weight: 900;
+        font-size: 28px;
+        text-shadow: 8px 8px 18px rgba(255, 255, 255, 0.9);
+
+        p {
+            font-size: 13px;
+        }
+
+        &:hover,
+        &:focus {
+            color: rgba(11, 68, 205, 1);
+            text-shadow: 8px 8px 18px rgba(0, 0, 0, 1);
+        }
+    }
+`,s="/rent-n-roll-auto/assets/cars_1-4AYW4PlE.webp",c="/rent-n-roll-auto/assets/hero-font-Ins461iM.jpg";function l(){return e.jsxs(n,{children:[e.jsxs(i,{font:s,children:[e.jsxs("h1",{children:["Welcome to",e.jsx("br",{})," ",e.jsx("span",{children:"Rent`N`Roll Auto"})]}),e.jsx("p",{children:"Your trusted partner for car rentals"})]}),e.jsxs(o,{children:[e.jsx("h2",{children:"About Us"}),e.jsx("p",{children:"Welcome to Rent`N`Roll Auto, your premier destination for reliable and affordable car rentals."}),e.jsx("p",{children:"At Rent`N`Roll Auto, we understand that convenience, flexibility, and quality are paramount when it comes to car rentals. Our mission is to provide you with a seamless and enjoyable experience from the moment you choose your vehicle to the completion of your journey."}),e.jsx("p",{children:"What sets us apart? It`s our commitment to customer satisfaction and our diverse fleet of well-maintained vehicles. Whether you need a compact car for a quick city trip, an SUV for a family adventure, or a luxury sedan for a special occasion, we have the perfect ride for you."}),e.jsx("p",{children:"Our team is dedicated to ensuring your safety and comfort. We prioritize regular maintenance, cleanliness, and customer service, so you can focus on the road ahead with peace of mind."}),e.jsx("p",{children:"Thank you for considering Rent`N`Roll Auto for your car rental needs. We look forward to being your trusted partner in every journey."})]}),e.jsxs(o,{children:[e.jsx("h2",{children:"Our Fleet"}),e.jsx("p",{children:"Explore our diverse fleet of vehicles, including sedans, SUVs, coupes, and more. Whether you need a car for a short trip or a long-term rental, we have the perfect option for you."})]}),e.jsx(o,{img:c,children:e.jsxs("h3",{children:[e.jsx("a",{href:"tel:+380730000000",children:"Contact Us"}),e.jsx("p",{children:"Have questions or ready to book a car? Contact our team for assistance."})]})})]})}function p(){return e.jsx(a,{children:e.jsx(l,{})})}export{p as default};
