6374b4dddffea6cd8b47e7d479369819d8e80171
Ljuzifer <Ljuzifer@users.noreply.github.com>
2023-12-10 18:53:14 +0000

    deploy: 6168fbcd6855b3c4c2300c38cb249c26dda774a0

---
+++ assets/NotFound-vImPoBeZ.js
@@ -0,0 +1,1 @@
+import{j as t,S as e}from"./index-nOEPXe0v.js";function i(){return t.jsx(e,{children:t.jsxs("div",{style:{textAlign:"center",marginLeft:"auto",marginRight:"auto",height:"100vh",paddingTop:200},children:[t.jsx("h1",{style:{fontSize:120},children:"404"}),t.jsx("b",{style:{fontSize:28},children:"Sorry, we couldn`t find that page..."})]})})}export{i as default};
