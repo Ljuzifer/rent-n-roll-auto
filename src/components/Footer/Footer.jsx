3347d1472b86702c61f1ed69bef30be0f5d7bc18
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-12-11 04:15:20 +0200

    more styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,18 @@
 import { FooterBlock } from "./Footer.styled";
+import { SiLinkedin } from "react-icons/si";
 
 export default function Footer() {
-    return <FooterBlock>&copy; 2023 Rent`N`Roll Auto. All rights reserved.</FooterBlock>;
+    return (
+        <FooterBlock style={{ whiteSpace: "pre" }}>
+            &copy; 2023 Rent`N`Roll Auto. All rights reserved.{"    "}Created by{" "}
+            <a
+                href="https://www.linkedin.com/in/ljuzifer/"
+                target="_blank"
+                rel="noreferrer noopener"
+                style={{ display: "flex", alignItems: "center" }}>
+                &copy; LJUZIFER{"    "}
+                <SiLinkedin style={{ fontSize: "20px" }} />
+            </a>
+        </FooterBlock>
+    );
 }
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,6 +2,7 @@ import { styled } from "styled-components";
 
 export const FooterBlock = styled.footer`
     display: flex;
+    justify-content: center;
     background-color: #333;
     color: #fff;
     text-align: center;
